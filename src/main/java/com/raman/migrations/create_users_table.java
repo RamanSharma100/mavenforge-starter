5b3ded3cc6d0505cf32a91f8634bbfb8034eb161
Raman Sharma <techraman100@gmail.com>
2024-07-04 23:49:40 +0530

    Level 1.4: Added Database Migrations

---
+++ src/main/java/com/raman/migrations/create_users_table.java
@@ -0,0 +1,19 @@
+package com.raman.migrations;
+
+import com.mavenforge.Schemas.Schema;
+
+public class create_users_table {
+    public void up() {
+        Schema.create("users", (table) -> {
+            table.id().autoIncrement().primary();
+            table.string("name");
+            table.string("email").unique();
+            table.integer("age").nullable();
+            table.timestamps();
+        });
+    }
+
+    public void down() {
+        // Drop the table
+    }
+}
