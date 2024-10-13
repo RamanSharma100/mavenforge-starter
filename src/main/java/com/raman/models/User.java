e9d1100c4fdcb4009c541da0385e2cc64672602a
Raman Sharma <techraman100@gmail.com>
2024-10-13 18:20:49 +0530

    Workng with models

--- src/main/java/com/raman/models/User.java
+++ src/main/java/com/raman/models/User.java
@@ -4,11 +4,21 @@ import com.mavenforge.Database.Model;
 
 public class User extends Model {
 
-    protected String[] fillable = {
-            "email"
-    };
+        /*
+         * The table associated with the model.
+         * The table name is automatically pluralized by the system unless you specify
+         * it.
+         * By default, the table name would be 'users' in this case.
+         * If you want to specify the table name, you can do so by adding the following
+         * line:
+         * protected String table = "users";
+         */
 
-    protected String[] gaurded = {
-            "password"
-    };
+        protected String[] fillable = {
+                        "email"
+        };
+
+        protected String[] guarded = {
+                        "password"
+        };
 }
